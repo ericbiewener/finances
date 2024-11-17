3300acc439562cb2b2175d41d37bd66dacbe1677
Eric Biewener <ebiewener@gmail.com>
2024-11-17 15:35:49 -0800

    more - Sun Nov 17 15:35:49 PST 2024

---
+++ src/accounts/data/401k.ts
@@ -0,0 +1,8 @@
+import { AccountData, Four01kAccountName } from "../types";
+
+export const four01kTotals: Record<Four01kAccountName, AccountData> = {
+  "401k": {
+    balance: 19060.44,
+    accountNum: "12473",
+  },
+};
--- src/accounts/types.ts
+++ src/accounts/types.ts
@@ -1,8 +1,36 @@
-export type RetirementAccountName = "401k" | "IRA" | "Roth" | "TRS";
+export const schwabRetirementAccountNames = ["IRA", "Roth", "TRS"] as const;
+export const schwabAccountsToIgnore = [
+  "Checking Brokerage",
+  "EF II - JOINT",
+  "LPS - JOINT",
+  "TRS - JOINT",
+  "Joint Checking",
+  "Checking - Joint",
+] as const;
+
+export const retirementAccountNames = [
+  "401k",
+  ...schwabRetirementAccountNames,
+] as const;
+
+export type SchwabRetirementAccountName = typeof schwabRetirementAccountNames;
+export type SchwabAccountsToIgnore = typeof schwabAccountsToIgnore;
+export type Four01kAccountName = "401k";
+export type RetirementAccountName = typeof retirementAccountNames;
 export type CheckingAccountName = "Checking";
-export type EmergencyFundAccountName = "EF";
+export type EmergencyFundAccountName = "EF II";
+
+export type SchwabLPSAccountName = "LPS";
+
+export type SchwabAccountName =
+  | SchwabRetirementAccountName
+  | CheckingAccountName
+  | EmergencyFundAccountName
+  | SchwabLPSAccountName;
 
 export type AccountName =
   | RetirementAccountName
   | CheckingAccountName
   | EmergencyFundAccountName;
+
+export type AccountData = { balance: number; accountNum: string };

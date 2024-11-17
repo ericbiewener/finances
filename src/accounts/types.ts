0753a70bc466e618b609805635977d970b80449c
Eric Biewener <ebiewener@gmail.com>
2024-11-17 14:01:49 -0800

    project init

---
+++ src/accounts/types.ts
@@ -0,0 +1,5 @@
+export type RetirementAccountName = "401k" | "IRA" | "Roth" | "TRS";
+export type CheckingAccountName = "Checking";
+export type EmergencyFundAccountName = "EF";
+
+export type AccountName = RetirementAccountName | CheckingAccountName | EmergencyFundAccountName;

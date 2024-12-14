39d2de1f274f7399403f3753c479d95d6e64f0a8
Eric Biewener <ebiewener@gmail.com>
2024-12-14 06:59:51 -0800

    more - Sat Dec 14 06:59:51 PST 2024

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // We don't want Next.js to decide what to lint. We want our `eslintrc` and
+  // `eslintignore` files to be the source of truth for that, since that is the
+  // standard setup in frontend codebases.
+  // https://nextjs.org/docs/pages/building-your-application/configuring/eslint#linting-custom-directories-and-files
+  eslint: {
+    dirs: ["."],
+  },
 };
 
 export default nextConfig;
